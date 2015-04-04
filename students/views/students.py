a4dec7feb42a9fbe0138c0ad6a405f6873601969
Sergiy Ignatenko <grydinywka@gmail.com>
2015-04-04 13:47:56 +0300

    add my pagination

--- students/views/students.py
+++ students/views/students.py
@@ -9,6 +9,13 @@ from ..models import Student
 # Views for Students
 def students_list(request):
 	students = Student.objects.all()
+	allStud = len(students)
+	valStudOnPage = 3
+	valPage = allStud/valStudOnPage
+	
+	if allStud % valStudOnPage != 0:
+		valPage += 1
+	listOfPage = [str(i) for i in xrange(1, valPage+1, 1)]
 
 	# try to order students list
 	order_by = request.GET.get('order_by', '')
@@ -19,20 +26,36 @@ def students_list(request):
 	elif order_by == '':
 		students = students.order_by('last_name')
 
-	#paginator students
-	paginator = Paginator(students, 3)
-	page = request.GET.get('page')
-	try:
-		students = paginator.page(page)
-	except PageNotAnInteger:
-		# If page is not an integer, deliver first page.
-		students = paginator.page(1)
-	except EmptyPage:
-		# If page is out of range (e. g. 9999), deliver
-		# last page of results.
-		students = paginator.page(paginator.num_pages)
+	# my paginator students
+	page = request.GET.get('page', '')
 	
-	return render(request, 'students/students_list.html', {'students': students})
+	try:
+		page = int(float(page))
+	except ValueError:
+		page = 1
+	if page > valPage or page < 1:
+		page = valPage
+	
+	large = valStudOnPage*page
+	little = large - valStudOnPage
+	students = students[little:large]
+
+	#paginator students
+	# paginator = Paginator(students, 3)
+	# page = request.GET.get('page')
+	# try:
+	# 	students = paginator.page(page)
+	# except PageNotAnInteger:
+	# 	# If page is not an integer, deliver first page.
+	# 	students = paginator.page(1)
+	# except EmptyPage:
+	# 	# If page is out of range (e. g. 9999), deliver
+	# 	# last page of results.
+	# 	students = paginator.page(paginator.num_pages)
+
+	return render(request, 'students/students_list.html', {'students': students,
+														   'valPage': valPage,
+														   'listOfPage': listOfPage})
 	
 	# return HttpResponse('<h1>Hello World!</h1>')
 
