f5613f4c77ca30f67e9a9c6631aa3ecc5d62fbad
Sergiy Ignatenko <grydinywka@gmail.com>
2016-04-10 11:34:21 +0300

    create logers for all models

--- students/signals.py
+++ students/signals.py
@@ -2,10 +2,10 @@
 
 import logging
 
-from django.db.models.signals import post_save, post_delete, m2m_changed
+from django.db.models.signals import post_save, post_delete, m2m_changed, pre_delete
 from django.dispatch import receiver
 
-from .models import Student, Group, MonthJournal, Exam
+from .models import Student, Group, MonthJournal, Exam, Result_exam
 
 def log_student_updated_added_event(sender, **kwargs):
 	"""Writes information about newly added or updated student info log file"""
@@ -67,23 +67,60 @@ def log_journal_delete_event(sender, **kwargs):
 	monthjournal = kwargs['instance']
 	logger.info("MonthJournal deleted: %s", monthjournal)
 
-# @receiver(post_save, sender=Exam)
-# def log_exam_change(sender, **kwargs):
-# 	logger = logging.getLogger(__name__)
-
-# 	exam = kwargs['instance']
-	
-# 	if kwargs['created']:
-# 		logger.info("Exam created: %s (ID: %d)", exam.title, exam.id)
-# 	else:
-# 		logger.info("Exam updated: %s (ID: %d)", exam.title, exam.id)
-
-def log_exam_update_add_event(sender, **kwargs):
+@receiver(post_save, sender=Exam)
+def log_exam_add_update_event(sender, **kwargs):
 	logger = logging.getLogger(__name__)
 
 	exam = kwargs['instance']
-	if kwargs.has_key('created'):
-		logger.info("Exam created: %s", exam)
+	
+	if kwargs['created']:
+		logger.info("Exam created: %s (ID: %d)", exam.title, exam.id)
 	else:
-		logger.info("Exam updated: %s", exam)
-m2m_changed.connect(log_exam_update_add_event, sender=Exam.exam_group.through)
+		logger.info("Exam updated: %s (ID: %d)", exam.title, exam.id)
+
+def log_group_of_exam_update_event(sender, **kwargs):
+	logger = logging.getLogger(__name__)
+
+	exam = kwargs['instance']
+	action = kwargs['action']
+
+	if action == "post_add":
+		info_str = "exam {} have next groups after modified:\n".format(exam)
+		for item in kwargs["pk_set"]:
+			group = kwargs["model"].objects.filter(pk=item)[0]
+			info_str += group.title + '\n'
+		logger.info(info_str)	
+m2m_changed.connect(log_group_of_exam_update_event, sender=Exam.exam_group.through)
+
+@receiver(post_delete, sender=Exam)
+def log_exam_delete_event(sender, **kwargs):
+	logger = logging.getLogger(__name__)
+
+	exam = kwargs['instance']
+	logger.info("Exam deleted: %s", exam)
+
+@receiver(post_delete, sender=Result_exam)
+def log_resultexam_delete_event(sender, **kwargs):
+	logger = logging.getLogger(__name__)
+
+	result_exam = kwargs['instance']
+	logger.info("Result_exam deleted(ID: %d)", result_exam.id)
+
+@receiver(post_save, sender=Result_exam)
+def log_resultexam_add_update_event(sender, **kwargs):
+	logger = logging.getLogger(__name__)
+
+	result_exam = kwargs['instance']
+	if kwargs['created']:
+		logger.info("Result_exam created(ID: %d)", result_exam.id)
+	else:
+		logger.info("Result_exam updated(ID: %d)", result_exam.id)
+
+@receiver(m2m_changed, sender=Result_exam.students.through)
+def log_resultexam_change_list_stud_event(sender, **kwargs):
+	logger = logging.getLogger(__name__)
+	result_exam = kwargs['instance']
+
+	if kwargs['action'] == 'post_add':
+		logger.info("List of students was modified \
+					 in result_exam {}".format(result_exam.id))
