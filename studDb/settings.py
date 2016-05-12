5a28a3cbdbe965bdb701dc3dad00a1f60e0bba1b
Sergiy Ignatenko <grydinywka@gmail.com>
2016-05-12 11:35:42 +0300

    fix JENKINS_TASKS

--- studDb/settings.py
+++ studDb/settings.py
@@ -207,7 +207,7 @@ LOGGING = {
     }
 }
 
-ENKINS_TASKS = ('django_jenkins.tasks.run_pylint',
+JENKINS_TASKS = ('django_jenkins.tasks.run_pylint',
                 'django_jenkins.tasks.run_pep8',
                 'django_jenkins.tasks.run_pyflakes',
                 'django_jenkins.tasks.with_coverage',
